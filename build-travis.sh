748b02f1279b6a21ca752b6bb3530f6dcba0d95c
Kamila Jacyniuk <kamilajacyniuk@gmail.com>
2017-04-18 21:13:03 +0200

    Add Travis build

---
+++ build-travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+gulp build
+git clone https://github.com/kamilajacyniuk/blumstudio
+cd blumstudio
+git checkout gh-pages && \
+cp -R ../dist/* .
+git config user.name "Kamila Jacyniuk"
+git config user.email kamilajacyniuk@gmail.com
+git add -A
+git commit -m "update gh-pages by Travis-CI"
+git push ${GIT_HUB_REPO_SECRET}
