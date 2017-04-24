4f4d19ec331651f7c169c95654e4c1ae9c687f60
Kamila Jacyniuk <kamilajacyniuk@gmail.com>
2017-04-24 20:26:37 +0200

    update build script

--- build-travis.sh
+++ build-travis.sh
@@ -7,6 +7,7 @@ git checkout gh-pages
 cp -R ../dist/* .
 git config user.name "Kamila Jacyniuk"
 git config user.email kamilajacyniuk@gmail.com
+git status
 git add .
-git commit -m "update gh-pages by Travis-CI"
-git push ${GIT_HUB_REPO_SECRET}
+git commit -m "update gh-pages by Travis-CI" || true
+git push ${GIT_HUB_REPO_SECRET} || true
