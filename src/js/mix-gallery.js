63fec4217a3e01ef706dddba626ad72313a15cac
Kamila Jacyniuk <kamilajacyniuk@gmail.com>
2017-04-26 21:48:55 +0200

    fix grid and add project house in mountains

--- src/js/mix-gallery.js
+++ src/js/mix-gallery.js
@@ -25,12 +25,3 @@ function getFilterFromHash() {
 							}
 					};
 			});
-
-			function myFunction() {
-			    var x = document.getElementById("menuResponsive");
-			    if (x.className === "menu-horizontal") {
-			        x.className += " responsive";
-			    } else {
-			        x.className = "menu-horizontal";
-			    }
-			}
